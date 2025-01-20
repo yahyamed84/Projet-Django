4b16b7593820c1edc486f06828a00c66aafbeaad
Mohamed Yahya YAHYA <23084@esp.mr>
2025-01-20 13:52:48 +0000

    chore: removed old version

--- app_ansade/project/pages/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-class PagesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pages'
-    verbose_name = 'Gestion des Pages'  # Nom lisible dans l'administration
